02bdf364c49d556f5bcd2d5b7076e2b1f7062de8
Stalanic <anustalanic@gmail.com>
2024-08-05 22:46:48 -0400

    mane.py moved

--- manage.py
+++ manage.py
@@ -1,12 +1,13 @@
 #!/usr/bin/env python
 """Django's command-line utility for administrative tasks."""
+# manage.py
+
 import os
 import sys
 
-
 def main():
     """Run administrative tasks."""
-    os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'rideTunes.settings')
+    os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'RideTunes.rideTunes.rideTunes.settings')
     try:
         from django.core.management import execute_from_command_line
     except ImportError as exc:
@@ -16,7 +17,6 @@ def main():
             "forget to activate a virtual environment?"
         ) from exc
     execute_from_command_line(sys.argv)
-
 
 if __name__ == '__main__':
     main()
