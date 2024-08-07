bee58cd71c045d143d1e16594c80ac8b6af05896
Stalanic <anustalanic@gmail.com>
2024-08-07 00:07:23 -0400

    CELERY_BROKER_URL

--- RideTunes/rideTunes/rideTunes/settings.py
+++ RideTunes/rideTunes/rideTunes/settings.py
@@ -55,7 +55,7 @@ SECURE_PROXY_SSL_HEADER = ('HTTP_X_FORWARDED_PROTO', 'https')
 SESSION_COOKIE_SECURE = True
 
 # Celery configurations
-CELERY_BROKER_URL = os.environ.get('CELERY_BROKER_URL', 'redis://localhost:6379/0')
+CELERY_BROKER_URL = os.environ.get('REDIS_URL', 'redis://localhost:6379/0')
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
